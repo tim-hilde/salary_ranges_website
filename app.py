45e52d1eb007e34af17a45c4c001eebf5e342e89
Tim Hildebrandt <44113468+tim-hilde@users.noreply.github.com>
2024-06-29 22:12:14 +0200

    auto use model

--- app.py
+++ app.py
@@ -5,17 +5,13 @@ st.write("# Vorhersage von Gehaltsspannen im Data Science Bereich")
 
 api_url = "http://127.0.0.1:8000"
 
-available_models = requests.get(f"{api_url}/models").json()
-
-model = st.selectbox("Wähle ein Model zur Vorhersage aus:", available_models)
-
 input_txt = st.text_area("Füge hier den gesamten Text der Stellenausschreibung ein:")
 
 if st.button("Vorhersagen!"):
     params = {
-        "model_name":model,
         "input": input_txt
     }
+
     prediction = requests.get(f"{api_url}/predict", params=params).json()
     range_min = prediction["range_min"]
     range_max = prediction["range_max"]
